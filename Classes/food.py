9111e1fb577693de33280c08ce819a8c7158b1b6
L9029 <angeloca03@gmail.com>
2024-01-30 20:01:06 -0400

    Writing some comments in the code of the cfg file and the class food

--- Classes/food.py
+++ Classes/food.py
@@ -5,16 +5,18 @@ class Food(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         self.screen_size = screen_size
-        self.images = images_dict[selected_key]
-        self.mask = pygame.mask.from_surface(self.images)
-        self.rect = self.images.get_rect()
+        self.images = images_dict[selected_key] #This select and image of the dict with the key
+        self.mask = pygame.mask.from_surface(self.images) #This detected the collision in base with the image of the food
+        self.rect = self.images.get_rect() #This delimited the position and the size of the image
         self.rect.left, self.rect.bottom = random.randint(20, screen_size[0] - 20), -10
-        self.speed = random.randrange(5, 10)
-        self.score = 1 if selected_key == "gold" else 5
+        self.speed = random.randrange(5, 10) #This represent the vertical speed for the food
+        self.score = 1 if selected_key == "gold" else 5 #This is the score associated with the food of the game like 1 for gold and 5 for others
     
     def update(self):
+        #This update the position of the food in any frame
         self.rect.bottom += self.speed
         
+        #This detects if the food get out of the bottom of the screen
         if self.rect.top > self.screen_size[1]:
             return True
         return False
--- Config/cfg.py
+++ Config/cfg.py
@@ -1,17 +1,20 @@
 import os
 
+#Path of the images for the game and hero
 IMAGE_PATH = {
     "golf": os.path.join(os.getcwd(), "resources/images/gold.png"),
     "background": os.path.join(os.getcwd(), "resources/images/background.png"),
     "apple": os.path.join(os.getcwd(), "resources/images/apple.png"),
-    "hero": [os.path.join(os.getcwd(), "resources/images/%d.png" % i) for i in range(1, 11)]
+    "hero": [os.path.join(os.getcwd(), "resources/images/%d.png" % i) for i in range(1, 11)] #this list comprehension takes the number of the images of the hero
 }
 
+#Path of the audios for the game
 AUDIO_PATH = {
     "coin_sound": os.path.join(os.getcwd(), "resources/audios/get.wav"),
     "music": os.path.join(os.getcwd(), "resources/audios/bgm.mp3")
 }
 
+#Path of the font for the game
 FONT_PATH = os.path.join(os.getcwd(), "resources/font/font.TTF")
 
 HIGHEST_SCORE_RECORD_FILEPATH = "highest_score.rec"
